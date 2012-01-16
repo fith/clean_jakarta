675cff08aedf222c1ec3ab975c57ef8bd82f0929
Kevin Mangan <fithmo@gmail.com>
2012-01-16 03:48:28 -0800

    styling comments

--- comment.tpl.php
+++ comment.tpl.php
@@ -2,34 +2,18 @@
 <?php print $pre; ?>
 
 <div <?php print drupal_attributes($attr); ?>>
-
-  <?php if ($title): ?>
-    <h2 class='comment-title'>
-      <?php if ($new): ?>
-        <a class="new">*</a>
-      <?php endif; ?>
-      <?php print $title; ?>
-    </h2>
-  <?php endif; ?>
-
-  <?php if ($submitted): ?>
-    <div class='comment-submitted clear-block'>
       <?php if ($submitted): ?>
         <div class='node-submitted clear-block'>
           <?php
-            if ($submitted) { 
               print 'by ' . theme('username', $node) . ' | ';
               print format_date($node->created, 'custom', 'F d, Y');
               if ($terms) {
                   print ' | in ' . $terms;
               }
             } 
-            ?>
+          ?>
         </div>
       <?php endif; ?>
-    </div>
-  <?php endif; ?>
-
   <?php print $picture; ?>
 
   <div class="comment-content clear-block">
