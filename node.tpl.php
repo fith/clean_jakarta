18f3443187f31468bb459c6b4facbf64c64a2f4c
Kevin Mangan <fithmo@gmail.com>
2012-01-20 03:16:38 -0800

    adding slogan

--- node.tpl.php
+++ node.tpl.php
@@ -28,7 +28,7 @@
               if($node->name !== "admin") {
                 print 'by ' . theme('username', $node) . ' | ';
               }
-              
+              print format_date($node->created, 'custom', 'F d, Y');
               if ($terms) {
                   print ' | in ' . $terms;
               }
