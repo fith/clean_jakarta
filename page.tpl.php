f40d6a4659156a5048ec739735bbb75256e0c7b4
Kevin Mangan <fithmo@gmail.com>
2012-01-26 02:11:01 -0800

    adding fbml thing to html tag

--- page.tpl.php
+++ page.tpl.php
@@ -1,7 +1,7 @@
 <?php // $Id: page.tpl.php,v 1.1.2.5.2.14.2.12 2010/03/01 13:37:46 psynaptic Exp $ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html<?php print drupal_attributes($html_attr); ?> prefix="og: http://ogp.me/ns#">
+<html<?php print drupal_attributes($html_attr); ?> prefix="og: http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml>
 
 <head>
   <meta name="google-site-verification" content="60LBlDHOJWoq6TemYgKCaUS6Vk9XnBc9kDMD0Ayzm9s" />
